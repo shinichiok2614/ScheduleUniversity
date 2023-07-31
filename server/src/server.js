95393a72d92de89d2dd89350b2b43a32a06cab8d
Tran Nguyen Minh Tuan <37470434+shinichiok2614@users.noreply.github.com>
2023-07-31 21:39:56 +0700

    connect success mysql and test pass

--- server/src/server.js
+++ server/src/server.js
@@ -1,9 +1,12 @@
 import express from 'express';
-import initRoute from './config/route';
+import initRoute from './routes/route';
+import connection from './config/connectDB';
 
 let app = express();
 
 initRoute(app);
+
+connection;
 
 let port = 8080;
 app.listen(port, () => {
