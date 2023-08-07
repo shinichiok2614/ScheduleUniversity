6a957614b8baa3046877edf906861c9b5719af75
Tran Nguyen Minh Tuan <37470434+shinichiok2614@users.noreply.github.com>
2023-08-07 10:08:17 +0700

    create student account success

--- server/src/controllers/authController.js
+++ server/src/controllers/authController.js
@@ -3,10 +3,42 @@ import argon2 from 'argon2';
 import jwt from 'jsonwebtoken';
 
 const db = require('../models');
-const register = async (req, res) => {};
+const register = async (req, res) => {
+  console.log('register');
+  const { username, password } = req.body;
+  if (!username || !password)
+    return res.status(400).json({
+      errCode: 2,
+      message: 'Missing username and/or password',
+    });
+  try {
+    const user = await db.student.findOne({
+      where: {
+        username,
+      },
+    });
+    if (user)
+      return res.status(401).json({
+        errCode: 3,
+        message: 'User already taken',
+      });
+    const hashpassword = await argon2.hash(password);
+    const newStudent = new db.student({ username, password: hashpassword });
+    await newStudent.save();
+    return res.status(200).json({
+      errCode: 0,
+      message: 'create student successfully',
+    });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({
+      errCode: 1,
+      message: 'Internal server error',
+    });
+  }
+};
+
 const login = async (req, res) => {
-  //   const data = await db.User.findOne();
-  console.log('login');
   const { username, password } = req.body;
   if (!username || !password)
     return res.status(400).json({
